0d97833aa23954c7c796db01ff7432f937e81009
Jeff Kesselman <jpkessel@purdue.edu>
2021-11-05 00:47:40 -0400

    button caps struct built

---
+++ src/HidLibrary/HIDButtonCaps.cs
@@ -0,0 +1,54 @@
+﻿namespace Hid.Net.Windows
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct RangeStruct
+    {
+        byte  UsageMin;
+        byte  UsageMax;
+        ushort StringMin;
+        ushort StringMax;
+        ushort DesignatorMin;
+        ushort DesignatorMax;
+        ushort DataIndexMin;
+        ushort DataIndexMax;
+        
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct NotRangeStruct
+    {
+        byte  Usage;
+        byte  Reserved1;
+        ushort StringIndex;
+        ushort Reserved2;
+        ushort DesignatorIndex;
+        ushort Reserved3;
+        ushort DataIndex;
+        ushort Reserved4;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    unsafe public struct HIDButtonCaps
+    {
+        [FieldOffset(0)]byte  UsagePage;
+        [FieldOffset(1)]byte  ReportID;
+        [FieldOffset(2)]byte  IsAlias; 
+        [FieldOffset(3)]ushort BitField;
+        [FieldOffset(5)]ushort LinkCollection;
+        [FieldOffset(7)]byte  LinkUsage;
+        [FieldOffset(8)]byte LinkUsagePage;
+        [FieldOffset(9)]byte IsRange;
+        [FieldOffset(10)]byte IsStringRange;
+        [FieldOffset(11)]byte IsDesignatorRange;
+        [FieldOffset(12)]byte IsAbsolute;
+        [FieldOffset(13)]ushort  ReportCount;
+        [FieldOffset(15)]ushort  Reserved2;
+        [FieldOffset(17)]fixed ulong Reserved[9]; // 4 bytes *9 = 36 bytes
+        // union
+        [FieldOffset(53)] private RangeStruct Range;
+        [FieldOffset(53)] private RangeStruct NotRange;
+    }
+}
---
+++ src/UnsafeUtils/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UnsafeUtils
+{
+    public static class UnsafeUtils
+    {
+        static unsafe bool ByteCompare<T>(T s1, T s2) where T : struct
+        {
+            var s1chars = Marshal.AllocHGlobal(Marshal.SizeOf(s1));
+            Marshal.StructureToPtr(s1,s1chars,false);
+            var s2chars = Marshal.AllocHGlobal(Marshal.SizeOf(s1));
+            Marshal.StructureToPtr(s1,s1chars,false);
+            var span1 = new Span<char>(s1chars.ToPointer(), Marshal.SizeOf(s1));
+            var span2 = new Span<char>(s2chars.ToPointer(), Marshal.SizeOf(s2));
+            bool result = MemoryExtensions.SequenceEqual(span1, span2);
+            Marshal.FreeHGlobal(s1chars);
+            Marshal.FreeHGlobal(s2chars);
+            return result;
+        }
+
+        static unsafe int HashCode<T>(T s) where T : struct
+        {
+            var size = Marshal.SizeOf(s);
+            var schars = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(s,schars,false);
+            int[] intArray = new int[(int) (Math.Ceiling((float) size) / 4)];
+            //var span = new Span<char>(schars.ToPointer(), Marshal.SizeOf(s));
+            Marshal.Copy(schars, intArray, 0, size);
+            HashCode hashCode = new HashCode();
+            foreach (var i in intArray)
+            {
+                hashCode.Add(i);
+            }
+            Marshal.FreeHGlobal(schars);
+            return hashCode.ToHashCode();
+        }
+    }
+    
+}
+
+public class Class1
+{
+}
